64b0ac1d35d146e0bc2f8eddb1e09aa76bb6b545
Isuru Bandara <isuru.bandara@ascentic.se>
2025-03-23 16:22:00 +0530

    Image URL for memebrs

--- LibraryManagement.Application/Features/Members/Dtos/GetAllMembersResponseDto.cs
+++ LibraryManagement.Application/Features/Members/Dtos/GetAllMembersResponseDto.cs
@@ -6,5 +6,6 @@
         public required string FullName { get; set; }
         public required string  Email { get; set; }
         public int NoOfBooksBorrowed { get; set; }
+        public string ImageURL { get; set; }
     }
 }
